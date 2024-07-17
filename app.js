5efad52e52b5e6f5b2ae6bff403aee2d2d371f9b
Anand Panda <148877270+anandpanda@users.noreply.github.com>
2024-07-17 16:26:51 +0530

    add dynamic port

--- app.js
+++ app.js
@@ -65,6 +65,8 @@ io.on("connection", function (uniqueSocket) {
 	});
 });
 
-server.listen(3000, () => {
-	console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT || 3000, () => {
+	console.log("Server is running on port PORT");
 });
