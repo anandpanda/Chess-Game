018ec5871cb2df7d8c211ffbbf7cea1b71757358
Anand Panda <anand612p@gmail.com>
2024-08-03 16:48:50 +0530

    dynamic port

--- app.js
+++ app.js
@@ -65,7 +65,7 @@ io.on("connection", function (uniqueSocket) {
 	});
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 server.listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
